0fb0536faf3da3f864541ad17419a9a096e523ae
James Wu <wuhy80@gmail.com>
2010-10-02 14:18:16 +0800

    添加了多语言支持
    另外，对样式做了一些调整

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,24 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   before_filter :authenticate_user!
+  before_filter :set_locale
+
+  def set_locale 
+    I18n.locale = params[:locale] 
+  end 
+
+  def default_url_options(options={})  
+    #logger.debug "default_url_options is passed options: #{options.inspect}\n"  
+    { :locale => I18n.locale } 
+  end 
+
+  layout :layout_by_resource
+
+  def layout_by_resource
+    if devise_controller?
+      "session_layout"
+    else
+      "application"
+    end
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,2 @@
 module ApplicationHelper
-  def date_diffs(dt)
-    return "#{time_ago_in_words(dt)} ago"
-  end
 end
