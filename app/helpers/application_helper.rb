51905aea5d31d5a5af7adecff212c25f4964f881
James Wu <wuhy80@gmail.com>
2010-10-19 22:23:11 +0800

    添加了用户图片这个功能。
    但是还存在一个问题，点击图片等内容的时候总是会有边框出现

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,20 @@ module ApplicationHelper
       return "女"
     end
   end
+
+  def user_name(user)
+    if user.profile.name.presence 
+      return user.profile.name
+    end  
+
+    return user.email
+  end
+
+  def user_pic(user)
+    if user.profile.avatar_file_name.presence
+      return current_user.profile.avatar.url
+    end
+
+    return "male.gif"
+  end
 end
