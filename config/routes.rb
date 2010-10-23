861995bb8594027c66447e8e85d74619c2a1cb12
James Wu <wuhy80@gmail.com>
2010-10-23 22:30:10 +0800

    已经解决了comment的问题，下面需要实现一些动画的效果

--- config/routes.rb
+++ config/routes.rb
@@ -43,7 +43,9 @@ MEET::Application.routes.draw do
     resources :schools
     resources :subjects
     resources :users
-    resources :courses
+    resources :courses  do
+      resources	:comments
+    end
   end
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
---
+++ public/javascripts/align.js
@@ -0,0 +1,17 @@
+﻿function alignHeight(eleA,eleB){
+    if(!document.getElementById(eleA)){return false;}
+    if(!document.getElementById(eleB)){return false;}
+    var heightA = document.getElementById(eleA).clientHeight;
+    var heightB = document.getElementById(eleB).clientHeight;
+    if(heightA > heightB){
+        document.getElementById(eleB).style.height = heightA + "px";
+        document.getElementById(eleA).style.height = heightA + "px";
+    }else{
+        document.getElementById(eleA).style.height = heightB + "px";
+        document.getElementById(eleB).style.height = heightB + "px";
+    }
+}
+
+window.onload = function z_align(){
+    alignHeight("sidebar_application","main");
+}
