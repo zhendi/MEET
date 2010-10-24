a1ee1a0c2d7ac640391d3ddd1b1184955ca84f77
James Wu <wuhy80@gmail.com>
2010-10-24 13:46:05 +0800

    无法解决comment在admin空间下的问题。干脆拷贝一份出来

---
+++ app/controllers/admin/comments_controller.rb
@@ -0,0 +1,38 @@
+﻿class Admin::CommentsController < ApplicationController
+  respond_to :html,:js
+
+  def index
+    @commentable = find_commentable
+    @comments = @commentable.comments
+  end
+
+  def create
+    @commentable = find_commentable
+    @comment = @commentable.comments.build(params[:comment])
+    if @comment.save
+      flash[:notice] = "Successfully created comment."
+    else
+      render :action => 'new'
+    end
+  end
+
+  def destroy
+    @commentable = find_commentable
+    @comment = @commentable.comments.find(params[:id])
+    @comment_id = "comment_#{@comment.id}"
+    if @comment.destroy
+      flash[:notice] = "Successfully deleted"
+    end
+  end
+
+  private
+
+  def find_commentable
+    params.each do |name, value|
+      if name =~ /(.+)_id$/
+        return $1.classify.constantize.find(value)
+      end
+    end
+    nil
+  end
+end
