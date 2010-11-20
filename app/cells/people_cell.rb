3d6fa758d79c8cf6454f048901964675e5b17faa
James Wu <wuhy80@qq.com>
2010-11-20 13:45:36 +0800

    将朋友关系里面的requested_friends, pending_friends以及connected_friends都使用cell的形式重做。这样方便将来的管理工作

--- app/cells/people_cell.rb
+++ app/cells/people_cell.rb
@@ -1,5 +1,5 @@
 class PeopleCell < Cell::Rails
-  helper ApplicationHelper
+  helper  ApplicationHelper
 
   def message_board
     render
@@ -27,4 +27,30 @@ class PeopleCell < Cell::Rails
     render
   end
 
+  def all_people
+    @users = User.find(:all)
+
+    render
+  end
+
+  def connected_friends
+    current_user = @opts[:current_user]
+    @connected_friends = Friendship.where("user_id=? and status=?", current_user, Friendship::ACCEPTED)
+
+    render
+  end
+
+  def requested_friends
+    current_user = @opts[:current_user]
+    @requested_friends = Friendship.where("friend_id=? and status=?", current_user, Friendship::REQUESTED)
+    
+    render
+  end
+
+  def pending_friends
+    current_user = @opts[:current_user]
+    @pending_friends = Friendship.where("user_id=? and status=?", current_user, Friendship::PENDING)
+
+    render
+  end
 end
