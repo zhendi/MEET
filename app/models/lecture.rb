6515c576e8a82daf94b2fb89e7887177fe3d630a
James Wu <wuhy80@qq.com>
2010-11-27 11:44:19 +0800

    去掉注册窗口

--- app/models/lecture.rb
+++ app/models/lecture.rb
@@ -2,3 +2,22 @@ class Lecture < ActiveRecord::Base
   belongs_to  :course
 
 end
+
+# == Schema Information
+#
+# Table name: lectures
+#
+#  id                  :integer         not null, primary key
+#  title               :string(255)
+#  video_src           :string(255)
+#  description         :text
+#  course_id           :integer
+#  level               :integer
+#  avatar_file_name    :string(255)
+#  avatar_content_type :string(255)
+#  avatar_file_size    :integer
+#  avatar_updated_at   :datetime
+#  created_at          :datetime
+#  updated_at          :datetime
+#
+
--- spec/models/lecture_spec.rb
+++ spec/models/lecture_spec.rb
@@ -3,3 +3,22 @@ require 'spec_helper'
 describe Lecture do
   pending "add some examples to (or delete) #{__FILE__}"
 end
+
+# == Schema Information
+#
+# Table name: lectures
+#
+#  id                  :integer         not null, primary key
+#  title               :string(255)
+#  video_src           :string(255)
+#  description         :text
+#  course_id           :integer
+#  level               :integer
+#  avatar_file_name    :string(255)
+#  avatar_content_type :string(255)
+#  avatar_file_size    :integer
+#  avatar_updated_at   :datetime
+#  created_at          :datetime
+#  updated_at          :datetime
+#
+
